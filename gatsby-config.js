2d3100994700435c6fc963ce6628d526c95315b7
mono motapa <bahobab@hotmail.com>
2020-04-20 19:33:13 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Ivoire cafe chaud`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@gatsbyjs`
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
+    `gatsby-plugin-react-helmet`, {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
+    `gatsby-plugin-sharp`, {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -24,11 +22,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
+      }
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    // this (optional) plugin enables Progressive Web App + Offline functionality To
+    // learn more, visit: https://gatsby.dev/offline `gatsby-plugin-offline`,
+  ]
 }
